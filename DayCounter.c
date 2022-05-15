0a0556d2e80840500d1d4d62cd3ff7bdc5fe43d1
FunctionalZ <Functional200@gmail.com>
2022-05-14 22:33:46 -0400

    commit on master

---
+++ DayCounter.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    int i;
+    for(i = 1; i < 86400; i++)
+    {
+        printf("Seconds since you opened the application: ");
+        printf("%d", i);
+        printf("\n");
+        sleep(1);
+        system("clear");
+    }
+
+    for(i = 5; i > 0; i--)
+    {
+        printf("It has been a day now so the program will close in aproximately: ");
+        printf("%d", i);
+        sleep(1);
+        system("clear");
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,5 @@
+This is a very simple comand line program written in C by FunctZii (twitch.tv/functzii)
+It counts the number of seconds over the course of aproximately one day
+Before counting down for 5 seconds and closing
+you can compile this using the GCC compiler
+The precompiled binary is included in this source
